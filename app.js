710740bab0ee277e5f33c76b749981263442d08d
Atolagbe Dotun <dotundotun20@gmail.com>
2023-06-27 01:20:44 +0100

    add security

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 require('express-async-errors');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit')
 const express = require('express');
 const app = express();
 
@@ -10,11 +14,21 @@ const authenticate = require('./middleware/authentication')
 const authRoute = require('./routes/auth');
 const jobsRoute = require('./routes/jobs')
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({windowMs:15 * 60 * 1000, max:100}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss())
 
+
+app.get('/', (req, res) => {
+  res.send('<h1>JOBS API</h1>');
+});
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
 
 
 app.use('/api/v1/auth', authRoute);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,29 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg:err.message || 'something went wrong, Try again'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.error).map((item) => item.message).join(','),
+    customError.statusCode = 400
+}
+
+
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value ${Object.keys(err.KeyValue)} field, please cgose another value`,
+      customError.statusCode = 400
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item with id: ${err.value}`,
+      customError.statusCode = 400 
+  }
+
+
+  return res.status(customError.statusCode).json({msg:customError.msg})
 }
 
 module.exports = errorHandlerMiddleware
